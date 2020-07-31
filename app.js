4c9d08623580967b8e7503357c75bdea94658d46
CrisT <crisostomotorres@hotmail.com>
2020-07-30 22:33:23 -0300

    renamed start script to dev-start

--- app.js
+++ app.js
@@ -29,13 +29,6 @@ mercadopago.configure({
 });
 
 //cors set up
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 //setup Express Application
 const app = express();
@@ -45,6 +38,13 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use("/api", routes);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 //inicializando aplicacion
 const port = process.env.PORT || 8080;
