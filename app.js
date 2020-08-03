7d1ec430c7cb95b3bd20f2a2c36f24cd465f5fa3
CrisT <crisostomotorres@hotmail.com>
2020-08-03 12:59:22 -0300

    Moved cors() to the top

--- app.js
+++ app.js
@@ -30,19 +30,13 @@ mercadopago.configure({
 
 //setup Express Application
 const app = express();
+app.use(cors())
 app.use(morgan("short"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use("/api", routes);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 //inicializando aplicacion
 const port = process.env.PORT || 8080;
